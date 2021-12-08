16a4248896341b45265b08cf367712db60c1f9c2
sephoenix <se.phoenix@gmail.com>
2021-12-08 20:10:17 +0100

    lab a ciegas

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) =>res.render('index'));
+
+router.get('/movies', (req, res)=>{
+    Movie.find({})
+    .then ((films)=>{
+        res.render('movies', {films})
+    })
+})
+
+router.get('/movies/:id', (req, res)=>{
+    const {id} = req.params;
+    Movie.findById(id)
+    .then((film)=>{
+        res.render('detail', {film})
+    })
+})
 
 module.exports = router;
